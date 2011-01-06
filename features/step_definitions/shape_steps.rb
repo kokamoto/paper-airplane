0716aae2705fdca77c169ee48bc6413a02b0625c
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2011-01-05 21:52:07 -0800

    Working on Shape features, and step definitions.

--- features/step_definitions/shape_steps.rb
+++ features/step_definitions/shape_steps.rb
@@ -1,9 +1,19 @@
-Given /^the Shape is a Rectangle of Width (\d+\.?\d*) cm and Length (\d+\.\d*) cm$/ do |width, length|
-  pending # express the regexp above with the code you wish you had
+require 'lib/shape'
+
+Given /^the Shape is a Rectangle$/ do
+  # pending # express the regexp above with the code you wish you had
 end
 
-Given /^the Shape is an Ellipse of Width (\d+\.?\d*) cm and Length (\d+\.\d*) cm$/ do |width, length|
-  pending # express the regexp above with the code you wish you had
+Given /^the Shape is an Ellipse$/ do
+  # pending # express the regexp above with the code you wish you had
+end
+
+Given /^has a Width of (\d+\.?\d*) cm$/ do |width|
+  @width = width.to_f
+end
+
+Given /^has a Height of (\d+\.?\d*) cm$/ do |height|
+  @height = height.to_f
 end
 
 Given /^the Shape is a Polygon with verticies at \((\d+),(\d+)\), \((\d+),(\d+)\), \((\d+),(\d+)\), \((\d+),(\d+)\)$/ do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
@@ -15,6 +25,6 @@ When /^area is requested$/ do
 end
 
 Then /^the Area should be (\d+\.?\d*) cm2$/ do |area|
-  pending # express the regexp above with the code you wish you had
+  area.to_f.should == @width * @height
 end
 
